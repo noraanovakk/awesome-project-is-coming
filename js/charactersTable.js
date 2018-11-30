bf5ca82ef97b0e0092c9c929dcac14d7157619b5
Kerklecz Médea <kerklecz.medea@gmail.com>
2018-11-30 16:37:31 +0100

    table 1.3 - good house and organization

--- js/charactersTable.js
+++ js/charactersTable.js
@@ -4,15 +4,21 @@ function characters() {
   for ( var i = 0; i < gameOfThronesCharacters.length; i += 1) {
     var house;
     if (gameOfThronesCharacters[i].house) {
-      house = gameOfThronesCharacters[i].house;
+      house = (`<img src="/img/houses/${gameOfThronesCharacters[i].house}.png" alt="housePicture"> ${gameOfThronesCharacters[i].house}`);
     } else {
       house = '';
+    }
+    var organization;
+    if (gameOfThronesCharacters[i].organization) {
+      organization = gameOfThronesCharacters[i].organization;
+    } else {
+      organization = '';
     }
     tableRow += `<tr>
                   <td>${gameOfThronesCharacters[i].name}</td>
                   <td><img src="${gameOfThronesCharacters[i].portrait}" alt="portrait"></td>
-                  <td><img src="/img/houses/${gameOfThronesCharacters[i].house}.png" alt="housePicture"> ${gameOfThronesCharacters[i].house}</td>
-                  <td>${gameOfThronesCharacters[i].organization}</td>
+                  <td>${house}</td>
+                  <td>${organization}</td>
                   <td>${gameOfThronesCharacters[i].bio}</td>
                   <td><input type="button" value="törlés" onclick="deleteLine()"></td>
                   <td><input type="button" value="módosítás" onclick="changeBio"></td>
