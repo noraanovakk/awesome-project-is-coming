14d54ba0f1f3b47386b586326545faf2e76fcd4f
Kerklecz Médea <kerklecz.medea@gmail.com>
2018-11-30 17:00:22 +0100

    good alias, house, organization

--- js/charactersTable.js
+++ js/charactersTable.js
@@ -2,6 +2,12 @@ function characters() {
   var characerTablesElement = document.querySelector('#character');
   var tableRow;
   for ( var i = 0; i < gameOfThronesCharacters.length; i += 1) {
+    var alias;
+    if (gameOfThronesCharacters[i].alias) {
+      alias = (`${gameOfThronesCharacters[i].name} \n alias: \n ${gameOfThronesCharacters[i].alias}`);
+    } else {
+      alias = (`${gameOfThronesCharacters[i].name}`);
+    }
     var house;
     if (gameOfThronesCharacters[i].house) {
       house = (`<img src="/img/houses/${gameOfThronesCharacters[i].house}.png" alt="housePicture"> ${gameOfThronesCharacters[i].house}`);
@@ -10,12 +16,12 @@ function characters() {
     }
     var organization;
     if (gameOfThronesCharacters[i].organization) {
-      organization = gameOfThronesCharacters[i].organization;
+      organization = (`${gameOfThronesCharacters[i].organization}`);
     } else {
       organization = '';
     }
     tableRow += `<tr>
-                  <td>${gameOfThronesCharacters[i].name}</td>
+                  <td>${alias}</td>
                   <td><img src="${gameOfThronesCharacters[i].portrait}" alt="portrait"></td>
                   <td>${house}</td>
                   <td>${organization}</td>
