ff916b00aedb78514ce29e2032c2ad7b72dc715b
Kerklecz Médea <kerklecz.medea@gmail.com>
2018-11-30 10:44:35 +0100

    charactersTable change

--- js/charactersTable.js
+++ js/charactersTable.js
@@ -3,6 +3,9 @@ var tableRow = `<tr>
                   <td>${arr[0].name}</td>
                   <td>${arr[0].portrait}</td>
                   <td></td>
+                  <td>${arr[0].house}</td>
+                  
+                  <td>${arr[0].portrait}</td>
                     
                 </tr>`
 ;
