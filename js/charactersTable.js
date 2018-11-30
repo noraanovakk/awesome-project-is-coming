eb38f6a01849f7482f42072790d24675010dd8de
Kerklecz Médea <kerklecz.medea@gmail.com>
2018-11-30 10:32:26 +0100

    add charactersTable first step

---
+++ js/charactersTable.js
@@ -0,0 +1,8 @@
+var characerTablesElement = document.querySelector('#character');
+var tableRow = `<tr>
+                  <td>${arr[0].name}</td>
+                  <td>${arr[0].portrait}</td>
+                  <td></td>
+                    
+                </tr>`
+;
