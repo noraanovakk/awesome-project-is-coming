75562341058f2b96232753c70bfc7f782356b23c
Kerklecz Médea <kerklecz.medea@gmail.com>
2018-11-30 11:04:53 +0100

    var tableRow creation

--- js/charactersTable.js
+++ js/charactersTable.js
@@ -1,11 +1,11 @@
 var characerTablesElement = document.querySelector('#character');
 var tableRow = `<tr>
-                  <td>${arr[0].name}</td>
-                  <td>${arr[0].portrait}</td>
-                  <td></td>
-                  <td>${arr[0].house}</td>
-                  
-                  <td>${arr[0].portrait}</td>
-                    
+                  <td>${gameOfThronesCharacters[0].name}</td>
+                  <td>${gameOfThronesCharacters[0].portrait}</td>
+                  <td><img src="/img/house/${gameOfThronesCharacters[0].house}.png" alt="housePicture"></td>
+                  <td>${gameOfThronesCharacters[0].house}</td>
+                  <td>${gameOfThronesCharacters[0].bio}</td>
+                  <td><input type="button" value="törlés" onclick="deleteLine()"></td>
+                  <td><input type="button" value="módosítás" onclick="changeBio"></td>
                 </tr>`
 ;
